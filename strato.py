85d6b8e2ef6f9616b8ad0e05bc2ed7ab27c8fc5f
Fabo <40230@pm.me>
2020-10-01 22:33:58 -0500

    Add files via upload

--- strato.py
+++ strato.py
@@ -6,26 +6,27 @@ from pandas import DataFrame
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 from freqtrade.strategy.interface import IStrategy
 
-"""
-Strato Strategy
-Designed by @fabohax
-CC0 License
-"""
 
 class strato(IStrategy):
+    """
+    Default Strategy provided by freqtrade bot.
+    Please do not modify this strategy, it's  intended for internal use only.
+    Please look at the SampleStrategy in the user_data/strategy directory
+    or strategy repository https://github.com/freqtrade/freqtrade-strategies
+    for samples and inspiration.
+    """
     INTERFACE_VERSION = 2
 
     # Minimal ROI designed for the strategy
     minimal_roi = {
-         "0": 0.004, "12": 0.002, "24": 0.001, "48": 0
+         "0": 0.0033, "5":0.002
     }
 
     # Optimal stoploss designed for the strategy
     stoploss = -0.0021
-    
 
     # Optimal ticker interval for the strategy
-    ticker_interval = '3m'
+    ticker_interval = '1m'
 
     # Optional order type mapping
     order_types = {
@@ -36,7 +37,7 @@ class strato(IStrategy):
     }
 
     # Number of candles the strategy requires before producing valid signals
-    startup_candle_count: int = 21
+    startup_candle_count: int = 20
 
     # Optional time in force for orders
     order_time_in_force = {
@@ -45,26 +46,53 @@ class strato(IStrategy):
     }
 
     def informative_pairs(self):
+        """
+        Define additional, informative pair/interval combinations to be cached from the exchange.
+        These pair/interval combinations are non-tradeable, unless they are part
+        of the whitelist as well.
+        For more information, please consult the documentation
+        :return: List of tuples in the format (pair, interval)
+            Sample: return [("ETH/USDT", "5m"),
+                            ("BTC/USDT", "15m"),
+                            ]
+        """
         return []
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        
+        """
+        Adds several different TA indicators to the given DataFrame
+
+        Performance Note: For the best performance be frugal on the number of indicators
+        you are using. Let uncomment only the indicator you are using in your strategies
+        or your hyperopt configuration, otherwise you will waste your memory and CPU usage.
+        :param dataframe: Dataframe with data from the exchange
+        :param metadata: Additional information, like the currently traded pair
+        :return: a Dataframe with all mandatory indicators for the strategies
+        """
+
         # Bollinger bands
-        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=21, stds=2.1)
+        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=21, stds=2.7)
+        dataframe['bblow'] = bollinger['lower']
         dataframe['bbhigh'] = bollinger['upper']
 
-        bollinger2 = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=21, stds=2.7)
-        dataframe['bblow'] = bollinger2['lower']
+        # MACD
+        macd = ta.MACD(dataframe)
+        dataframe['macd'] = macd['macd']
+        dataframe['macdsignal'] = macd['macdsignal']
+        dataframe['macdhist'] = macd['macdhist'] 
 
         return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         """
-        BUY
+        Based on TA indicators, populates the buy signal for the given dataframe
+        :param dataframe: DataFrame
+        :param metadata: Additional information, like the currently traded pair
+        :return: DataFrame with buy column
         """
         dataframe.loc[
             (
-                ((dataframe['close']+dataframe['low'])/2) < dataframe['bblow']
+                (dataframe['close'] < dataframe['bblow']) & (dataframe['macdsignal']<dataframe['macdhist'])
             ),
             'buy'] = 1
 
@@ -72,12 +100,18 @@ class strato(IStrategy):
 
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         """
-        SELL
+        Based on TA indicators, populates the sell signal for the given dataframe
+        :param dataframe: DataFrame
+        :param metadata: Additional information, like the currently traded pair
+        :return: DataFrame with buy column
         """
         dataframe.loc[
             (
-                ((dataframe['close']+dataframe['high'])/2) > dataframe['bbhigh']
+                (dataframe['macdsignal'] > dataframe['macdhist']) | (dataframe['high'] > dataframe['bbhigh'])
             ),
             'sell'] = 1
         return dataframe
+
+
+
 
