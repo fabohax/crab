0fc42e058de1e1b0eb47ca036577afa575855d82
Fabo <40230@pm.me>
2021-03-08 00:47:14 -0500

    Add files via upload

--- strato.py
+++ strato.py
@@ -11,8 +11,7 @@ class strato(IStrategy):
     INTERFACE_VERSION = 2
 
     minimal_roi = {
-        "0": 0.0055,
-        "30": 0
+        "0": 0.021
     }
 
     stoploss = -0.1
@@ -21,18 +20,13 @@ class strato(IStrategy):
     timeframe = '1m'
 
     order_types = {
-        'buy': 'limit',
+        'buy': 'market',
         'sell': 'market',
         'stoploss': 'market',
         'stoploss_on_exchange': False
     }
 
     startup_candle_count: int = 20
-
-    trailing_stop = True
-    trailing_stop_positive = 0.13267
-    trailing_stop_positive_offset = 0.22438
-    trailing_only_offset_is_reached = True
 
     order_time_in_force = {
         'buy': 'gtc',
@@ -48,30 +42,16 @@ class strato(IStrategy):
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
-        # Bollinger bands
-        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=21, stds=2.7)
-        dataframe['bblo'] = bollinger['lower']
-        dataframe['bbmi'] = bollinger['mid']
-
-        bollinger2 = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=21, stds=1.1)
-        dataframe['bbhi'] = bollinger2['upper']
-
-        # MACD
-        macd = ta.MACD(dataframe)
-        dataframe['macd'] = macd['macd']
-        dataframe['macdsignal'] = macd['macdsignal']
-        dataframe['macdhist'] = macd['macdhist']     
-
         # #RSI
         dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
         
         # #StochRSI 
-        period = 14
-        smoothD = 3
-        SmoothK = 3
-        stochrsi  = (dataframe['rsi'] - dataframe['rsi'].rolling(period).min()) / (dataframe['rsi'].rolling(period).max() - dataframe['rsi'].rolling(period).min())
-        dataframe['srsi_k'] = stochrsi.rolling(SmoothK).mean() * 100
-        dataframe['srsi_d'] = dataframe['srsi_k'].rolling(smoothD).mean()
+        p = 14
+        d = 3
+        k = 3
+        srsi  = (dataframe['rsi'] - dataframe['rsi'].rolling(p).min()) / (dataframe['rsi'].rolling(p).max() - dataframe['rsi'].rolling(p).min())
+        dataframe['k'] = srsi.rolling(k).mean() * 100
+        dataframe['d'] = dataframe['k'].rolling(d).mean()
 
 
         return dataframe
@@ -80,7 +60,8 @@ class strato(IStrategy):
 
         dataframe.loc[
             (
-                (dataframe['low'] < dataframe['bblo']) & (dataframe['srsi_d']<7) & (dataframe['macd']<0)
+                (dataframe['k'] < 20) &
+                (dataframe['k'] > dataframe['d'])
 	        ),
             'buy'] = 1
 
@@ -90,7 +71,8 @@ class strato(IStrategy):
 
         dataframe.loc[
             (
-                (dataframe['high'] > dataframe['bbmi'])
+                (dataframe['d'] > 80) &
+                (dataframe['d'] > dataframe['k'])
             ),
             'sell'] = 1
         return dataframe
