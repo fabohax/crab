da149303f04e0b6874c07c35eae8b335bffd51c3
Fabo <40230@pm.me>
2023-12-07 10:20:06 -0500

    Update crub.rb
    sideshift integration fixes

--- crub.rb
+++ crub.rb
@@ -1,30 +1,54 @@
 require 'httparty'
-require 'dotenv'
-require 'rufus-scheduler'
-require 'logger'
 
-response = HTTParty.get('https://sideshift.ai/api/v1/market-info')
-puts response.body, response.code, response.message, response.headers.inspect
+# SIDESHIFT API credentials
+SIDESHIFT_API_URL = "https://api.sideshift.ai/v1/"
+ACCESS_KEY = "YOUR_ACCESS_KEY"
+SECRET_KEY = "YOUR_SECRET_KEY"
 
-Dotenv.load
-okex_client = OKEX::Client.new(api_key: ENV['OKEX_API_KEY'], api_secret: ENV['OKEX_API_SECRET'], api_passphrase: ENV['OKEX_API_PASSPHRASE'])
-sideshift_client = SideshiftAPI::Client.new(api_key: ENV['SIDESHIFT_API_KEY'], api_secret: ENV['SIDESHIFT_API_SECRET'])
+# User-defined variables
+BUY_AMOUNT_USDT = 100 # USDT amount to purchase Bitcoin with
+CURRENCY_PAIR = "BTC_USDT" # BTC/USDT currency pair
 
-scheduler = Rufus::Scheduler.new
-scheduler.every '15s' do
-  ticker = okex_client.ticker
-  puts "OKEX Ticker data: #{ticker}"
+# Cron job schedule
+CRON_SCHEDULE = "0 0 * * SUN" # Run script every Sunday at midnight
+
+# Helper function to make API calls
+def sideshift_api_call(path, method: :get, body: nil)
+  headers = {
+    "Authorization" => "Bearer #{ACCESS_KEY}:#{SECRET_KEY}",
+    "Content-Type" => "application/json"
+  }
+
+  response = HTTParty.send(method, "#{SIDESHIFT_API_URL}#{path}", headers: headers, body: body.to_json)
+  raise "API Error: #{response.body}" unless response.success?
+
+  response.parsed_response
 end
 
-def run_sideshift_bot(client)
-  logger = Logger.new('sideshift_logfile.log')
-  logger.datetime_format = '%Y-%m-%d %H:%M:%S'
-  logger.info("#{Time.now.strftime(logger.datetime_format)}: Running crub...")
-  
-  # SIDESHIFT bot code
-end
+# Fetch Bitcoin exchange rate
+exchange_rate = sideshift_api_call("rates/#{CURRENCY_PAIR}")["rate"]
 
-loop do
-  sleep 1
-  run_sideshift_bot(sideshift_client)
+# Calculate Bitcoin amount to purchase based on exchange rate
+btc_amount = BUY_AMOUNT_USDT / exchange_rate
+
+# Place Bitcoin purchase order
+order_response = sideshift_api_call("orders", method: :post, body: {
+  sourceAmount: BUY_AMOUNT_USDT,
+  sourceCurrency: "USDT",
+  destinationCurrency: "BTC",
+  destinationAmount: btc_amount
+})
+
+# Print order details
+puts "Bitcoin purchase order placed successfully!"
+puts "Order ID: #{order_response['orderId']}"
+puts "Source amount: #{order_response['sourceAmount']} USDT"
+puts "Destination amount: #{order_response['destinationAmount']} BTC"
+puts "Exchange rate: #{exchange_rate}"
+
+# Run script with Cron
+cron = Cron.new
+cron.schedule(CRON_SCHEDULE) do
+  system('ruby buy_bitcoin.rb')
 end
+cron.run
